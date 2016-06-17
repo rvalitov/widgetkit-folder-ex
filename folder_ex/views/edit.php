1a86f932eaa28f38f32df1174ce78834fd230c39
Ramil Valitov <ramilvalitov@gmail.com>
2016-06-17 03:15:37 +0200

    [fix] added missing translations

--- folder_ex/views/edit.php
+++ folder_ex/views/edit.php
@@ -18,21 +18,21 @@ require_once(__DIR__.'/helper.php');
 	</div>
 
 	<div class="uk-form-row">
-		<span class="uk-form-label" for="wk-path">{{ 'Folder Path' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="The folder where the images are stored. This field is mandatory."><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
+		<span class="uk-form-label" for="wk-path">{{ 'Folder Path' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="{{ 'The folder where the images are stored. This field is mandatory.' |trans}}"><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
 		<div class="uk-form-controls">
 			<input id="wk-path" class="uk-form-width-large" type="text" ng-model="content.data['folder']">
 		</div>
 	</div>
 
 	<div class="uk-form-row">
-		<span class="uk-form-label" for="wk-regexp">{{ 'RegExp Pattern' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="RegExp string used for filtering files. It allows to select only files that match the pattern. If this string is empty, then a default pattern is used that matches images with the following extensions: jpg, jpeg, gif, png. Read manual for more information."><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
+		<span class="uk-form-label" for="wk-regexp">{{ 'RegExp Pattern' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="{{ 'RegExp string used for filtering files. It allows to select only files that match the pattern. If this string is empty, then a default pattern is used that matches images with the following extensions: jpg, jpeg, gif, png. Read manual for more information.' |trans}}"><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
 		<div class="uk-form-controls">
 			<input id="wk-regexp" class="uk-form-width-large" type="text" ng-model="content.data['regexp']">
 		</div>
 	</div>
 	
 	<div class="uk-form-row">
-		<span class="uk-form-label" for="wk-order">{{'Order' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="This field defines the sorting options applied to the files."><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
+		<span class="uk-form-label" for="wk-order">{{'Order' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="{{ 'This field defines the sorting options applied to the files.' |trans}}"><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
 		<div class="uk-form-controls">
 			<select id="wk-order" class="uk-form-width-large" ng-model="content.data['sort_by']">
 				<option value="filename_asc">{{'Alphabetical' | trans}}</option>
@@ -45,7 +45,7 @@ require_once(__DIR__.'/helper.php');
 	</div>
 
 	<div class="uk-form-row">
-		<span class="uk-form-label">{{'Title' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="The title of each file is generated automatically from the file name. The options below control how the file name should be processed and altered."><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
+		<span class="uk-form-label">{{'Title' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="{{ 'The title of each file is generated automatically from the file name. The options below control how the file name should be processed.' |trans}}"><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
 		<div class="uk-form-controls uk-form-controls-text">
 			<p class="uk-form-controls-condensed">
 				<label><input type="checkbox" ng-model="content.data['strip_leading_numbers']"> {{'Remove leading numbers from title' | trans}}</label>
@@ -63,7 +63,7 @@ require_once(__DIR__.'/helper.php');
 	</div>
 
 	<div class="uk-form-row">
-		<span class="uk-form-label" for="wk-max-images">{{'Max Images' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="This option allows to limit the number of images to select. If this option is empty, then no restriction to the number of images is applied."><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
+		<span class="uk-form-label" for="wk-max-images">{{'Max Images' | trans}}<span  data-uk-tooltip style="margin-top: 5px;" title="{{ 'This option allows to limit the number of images to load. If this option is empty, then no restriction to the number of images is applied.' |trans}}"><i class="uk-icon uk-icon-question-circle uk-margin-small-left" style="color:#ffb105"></i></span></span>
 		<div class="uk-form-controls">
 			<input id="wk-max-images" class="uk-form-width-large" type="text" ng-model="content.data['max_images']" placeholder="{{ 'Leave empty to load all images' | trans }}" >
 		</div>
